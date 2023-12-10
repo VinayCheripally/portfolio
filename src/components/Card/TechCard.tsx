b3fa25a2e98bb7d1d219506d34b055f039e8c38c
VinayCheripally <vinaycheripally1@gmail.com>
2023-12-10 10:50:51 +0530

    add

--- src/components/Card/TechCard.tsx
+++ src/components/Card/TechCard.tsx
@@ -1,7 +1,7 @@
-import { Image, ImageIcon } from 'lucide-react';
-import { FC } from 'react';
-import { cn } from '../utils/utils';
-import Button from '../ui/Button';
+import { Image, ImageIcon } from "lucide-react";
+import { FC } from "react";
+import { cn } from "../utils/utils";
+import Button from "../ui/Button";
 
 interface TechCardProps {
   title: string;
@@ -20,23 +20,23 @@ const TechCard: FC<TechCardProps> = ({ title, classaName, tech }) => {
           <div
             key={i}
             className={cn(
-              'rounded-md bg-body text-xs text-primary p-2',
-              item === 'Tailwind' && 'border border-blue-500/60',
-              item === 'Javascript' && 'border border-yellow-500/60',
-              item === 'React Js' && 'border border-blue-500/60',
-              item === 'Next Js 13' && 'border border-black/60',
-              item === 'Node Js' && 'border border-green-500/60',
-              item === 'Express Js' && 'border border-gray-500/60',
-              item === 'MySql' && 'border border-blue-500/60',
-              item === 'MongoDB' && 'border border-emerald-500/60',
-              item === 'PostgreSQL' && 'border border-blue-500/60',
-              item === 'Redis' && 'border border-red-500/60',
-              item === 'Elastic Search' && 'border border-yellow-500/60',
-              item === 'Docker' && 'border border-blue-500/60',
-              item === 'Kubernetes' && 'border border-blue-500/60',
-              item === 'GIT' && 'border border-orange-500/60',
-              item === 'Terraform' && 'border border-purple-500/60',
-              item === 'AWS' && 'border border-yellow-500/60'
+              "rounded-md bg-body text-xs text-primary p-2",
+
+              item === "Javascript" && "border border-yellow-500/60",
+              item === "React Js" && "border border-blue-500/60",
+              item === "Next Js 13" && "border border-black/60",
+              item === "Node Js" && "border border-green-500/60",
+              item === "Express Js" && "border border-gray-500/60",
+              item === "MySql" && "border border-blue-500/60",
+              item === "MongoDB" && "border border-emerald-500/60",
+              item === "PostgreSQL" && "border border-blue-500/60",
+              item === "Redis" && "border border-red-500/60",
+              item === "Elastic Search" && "border border-yellow-500/60",
+              item === "Docker" && "border border-blue-500/60",
+              item === "Kubernetes" && "border border-blue-500/60",
+              item === "GIT" && "border border-orange-500/60",
+              item === "Terraform" && "border border-purple-500/60",
+              item === "AWS" && "border border-yellow-500/60"
             )}
           >
             {item}
